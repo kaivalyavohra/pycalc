8303ef6ebccd7f08beb9d17eec938b5712de0c23
kaivalyavohra <kaivalyavohra@gmail.com>
2019-03-01 11:00:05 +0400

    Add files via upload

---
+++ pycalc.py
@@ -0,0 +1,42 @@
+import tkinter
+
+
+class CalcFrame(tkinter.Frame):
+    def __init__(self, root):
+        tkinter.Frame.__init__(self)
+        root.title('Calculator')
+
+        self.entryText = tkinter.StringVar()
+        self.e = tkinter.Entry(root, textvariable=self.entryText, width=37,bd=12)
+        self.e.grid(row=0, column=0, columnspan=3)
+        ac = tkinter.Button(root, text='AC', command=lambda: self.entryText.set(
+            ""), padx=50, pady=12, width=2).grid(row=0, column=3)
+        buttons = ['789+', '456-', '123*', '0=^/']
+        col, row = 0, 1
+        for i in buttons:
+            for j in i:
+                if j != '^' and j != '=':
+                    B=tkinter.Button(root,text=j,command=lambda j=j:self.update(
+                        j), padx=50, pady=12, width=2).grid(row=row,column=col)
+
+                elif j == '^':
+                    B = tkinter.Button(root, text=j, command=lambda: self.update(
+                        '**'), padx=50, pady=12, width=2).grid(row=row, column=col)
+                else:
+                    B = tkinter.Button(root, text=j, command=lambda: self.calc(
+                    ), padx=50, pady=12, width=2).grid(row=row, column=col)
+                col += 1
+
+            row += 1
+            col = 0
+
+    def update(self, val):
+        self.entryText.set(self.e.get() + val)
+
+    def calc(self):
+        self.entryText.set(eval(self.e.get()))
+
+
+root = tkinter.Tk()
+calc = CalcFrame(root)
+root.mainloop()
